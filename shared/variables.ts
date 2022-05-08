3f77109e5175ee6c3bd67e71bf755888051782ea
martins86 <martins86@gmail.com>
2022-05-08 18:07:46 +0000

    detalhando as variáveis

--- shared/variables.ts
+++ shared/variables.ts
@@ -1,18 +1,31 @@
+/**
+ * Arquivo com as variaveis mais ultilizadas.
+ * Parametros com a url da rota antiga e a nova.
+ */
 const env = {
   old: "http://viacep.com.br/ws",
   new: "http://viacep.com.br/ws",
 };
 
+/**
+ * Parametros para url nos testes.
+ */
 let routeTest = {
   old: `${env.old}/`,
   new: `${env.new}/`,
 };
 
+/**
+ * Parametros para o retorno nos testes.
+ */
 let response = {
   old: null,
   new: null,
 };
 
+/**
+ * Função para limpar o retorno após o uso.
+ */
 const resetValues = () => {
   response = {
     old: null,
